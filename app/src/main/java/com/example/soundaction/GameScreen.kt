53634bbafef606816d9902ec800f383020b7c806
lemonade-2061 <lemonenergy.2061.7.29@gmail.com>
2025-11-07 11:24:20 +0900

    feat-修正とゲーム画面のUI

---
+++ app/src/main/java/com/example/soundaction/GameScreen.kt
@@ -0,0 +1,137 @@
+package com.example.soundaction
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import com.example.soundaction.ui.theme.AppTheme
+import androidx.compose.material.icons.filled.Pause
+
+@Composable
+fun GameScreen() {
+    val gradientStart = AppTheme.colors.accentGradientStart
+    val gradientEnd = AppTheme.colors.accentGradientEnd
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        gradientStart,
+                        gradientEnd
+                    )
+                )
+            )
+    ) {
+    //レーンの描画
+    Line()
+
+    IconButton(
+        onClick = {},
+        modifier = Modifier
+            .align(Alignment.TopEnd)
+            .padding(15.dp)
+            .background(Color.White.copy(alpha = 0.6f), shape = androidx.compose.foundation.shape.CircleShape)
+            .size(40.dp)
+    ) {
+        Icon(Icons.Filled.Pause, contentDescription = "Pause")
+    }
+        Column(
+                modifier = Modifier
+        ) {
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(4f)
+            ) {
+
+            }
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(1f)
+                    .background(Color.Black.copy(alpha = 0.2f))
+            ) {
+
+            }
+        }
+    }
+}
+
+@Composable
+fun Line() {
+    val dividerColor = Color.White.copy(alpha = 0.7f)
+    val dividerWidth = 1.dp
+    Row (
+        modifier = Modifier.fillMaxSize()
+    ){
+        //レーン 1
+        Box(
+            Modifier
+                .weight(1f)
+                .fillMaxHeight()
+        )
+
+        //境界線 1
+        Spacer(
+            modifier = Modifier
+                .fillMaxHeight()
+                .width(dividerWidth)
+                .background(dividerColor)
+        )
+
+        //レーン 2
+        Box(
+            Modifier
+                .weight(1f)
+                .fillMaxHeight()
+        )
+
+        // 境界線 2
+        Spacer(
+            modifier = Modifier
+                .fillMaxHeight()
+                .width(dividerWidth)
+                .background(dividerColor)
+        )
+
+        //レーン 3
+        Box(
+            Modifier
+                .weight(1f)
+                .fillMaxHeight()
+        )
+
+        // 境界線 3
+        Spacer(
+            modifier = Modifier
+                .fillMaxHeight()
+                .width(dividerWidth)
+                .background(dividerColor)
+        )
+
+        // レーン 4
+        Box(
+            Modifier
+                .weight(1f)
+                .fillMaxHeight()
+        )
+    }
+}
--- app/src/main/java/com/example/soundaction/MainActivity.kt
+++ app/src/main/java/com/example/soundaction/MainActivity.kt
@@ -4,18 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Button
@@ -27,19 +23,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontVariation.width
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.font.FontWeight.Companion.Bold
-import androidx.compose.ui.text.googlefonts.Font
-import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.soundaction.ui.theme.AppTheme
 import com.example.soundaction.ui.theme.SoundActionTheme
 object FontLoader {
     val googleFontFamily by lazy {
@@ -59,7 +49,7 @@ class MainActivity : ComponentActivity() {
                     Column(
                         modifier = Modifier.padding(innerPadding)
                     ) {
-                        MyVerticalLayout()
+                        GameScreen()
                     }
                 }
             }
@@ -69,25 +59,33 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyVerticalLayout() {
-    Box(
+    val gradientStart = AppTheme.colors.gradientStart
+    val gradientEnd = AppTheme.colors.gradientEnd
 
-    ){
-        Column (
-            modifier = Modifier
-                .fillMaxSize()
-                .background(
-                    brush = Brush.verticalGradient(
-                        colors = listOf(Color(0xFFD19BEA), Color(0xFFA4CCED))
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(
+                brush = Brush.verticalGradient(
+                    colors = listOf(
+                        gradientStart,
+                        gradientEnd
                     )
                 )
-        ) {
-
-        }
+            )
+    ) {
+            IconButton(
+                onClick = {},
+                modifier = Modifier.align(Alignment.TopStart).padding(20.dp)
+            ) {
+                Icon(Icons.Default.Settings, contentDescription = "設定")
+            }
         Column(
-            modifier = Modifier.padding(16.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
+            modifier = Modifier.align(Alignment.Center)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceEvenly
         ) {
-            Spacer(Modifier.height(100.dp))
             Text(
                 "Android-two",
                 fontSize = 45.sp,
@@ -95,15 +93,13 @@ fun MyVerticalLayout() {
                 textAlign = TextAlign.Center,
                 fontFamily = FontLoader.googleFontFamily,
             )
-            Spacer(Modifier.height(150.dp))
             Text(
                 text = "🎹",
                 fontSize = 150.sp,
             )
-            Spacer(Modifier.height(150.dp))
             Button(
 
-                onClick = { /* アクション */ },
+                onClick = {},
                 modifier = Modifier.size(width = 150.dp, height = 70.dp)
             ) {
                 Text(
@@ -113,22 +109,13 @@ fun MyVerticalLayout() {
                 )
             }
         }
-        Row(
-
-        ) {
-            Spacer(modifier = Modifier.width(20.dp))
-            IconButton(
-                onClick = {},
-                modifier = Modifier.height(100.dp)
-            ) {
-                Icon(Icons.Default.Settings, contentDescription = "ohaoha")
-            }
-        }
     }
-    }
+}
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-
+fun StartScreenPreview() {
+    SoundActionTheme {
+        MyVerticalLayout()
+    }
 }
