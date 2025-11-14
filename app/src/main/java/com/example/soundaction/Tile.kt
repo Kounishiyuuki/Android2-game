1fc1de067f30a9f2e42e13737ea5c9eb3b3c8efc
hayokbt <mihokubota@outlook.jp>
2025-11-14 13:15:56 +0900

    Refactor imports in Tile.kt for clarity

--- app/src/main/java/com/example/soundaction/Tile.kt
+++ app/src/main/java/com/example/soundaction/Tile.kt
@@ -7,9 +7,13 @@ import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.*
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clipToBounds
 import androidx.compose.ui.unit.Dp
